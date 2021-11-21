d6950c15c6bf7dd2d3353935f92c5366bf5d0ff9
Sandy Cordovi <scgonzalez@eandsolutions.com>
2021-11-21 18:06:20 -0500

    asd

---
+++ src/Chat.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import { View, Text } from "react-native";
+
+interface ChatProps {}
+
+const Chat: FC<ChatProps> = ({}) => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default Chat;
